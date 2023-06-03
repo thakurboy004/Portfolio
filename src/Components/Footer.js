ff588968e44f812c64f4c0b25f68f596c665e1f3
thakurboy004 <vicky986847@gmail.com>
2023-06-03 15:17:27 +0530

    Initializing my portfolio

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Footer = () => {
+  return (
+    <Footer>
+        <Layout>
+            <span>
+                Rights
+            </span>
+        </Layout>
+    </Footer>
+  )
+}
+
+export default Footer
