972f5ab6670660755811ab117aff059fe3a3ae2b
thakurboy004 <vicky986847@gmail.com>
2024-06-28 19:59:41 +0530

    Added About me in my portfolio ✔

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
+#     path('projects/', views.projects, name='projects'),
+#     path('skills/', views.skills, name='skills')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# def projects(request):
+#     return render(request, 'projects.html')
+
+# def skills(request):
+#     return render(request, 'skills.html')
